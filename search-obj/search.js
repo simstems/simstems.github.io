4e95e4afc3990db7f7c211c2e09ffd5106f97583
Stephen Morrison <simstems@users.noreply.github.com>
2017-04-21 23:03:52 -0400

    Delete search.js

--- search-obj/search.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#search-button").click(function(){
-       $("#srch-close-btn, #search-bar, #search-button, #search-drawer").addClass("show"); 
-    });
-    $("#srch-close-btn, .menu-button").click(function(){
-       $("#srch-close-btn, #search-bar, #search-button, #search-drawer").removeClass("show"); 
-    });
-});
