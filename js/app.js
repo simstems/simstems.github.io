59b3094571405d51ccd5b8d34a3f1082a5111a5b
Stephen Morrison <simstems@users.noreply.github.com>
2017-05-27 13:58:40 -0400

    Final Layout
    This is the final layout update for while.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+$(document).ready(function(){
+    if($(window).innerWidth() <= 751) {
+       $(".stephen .contact").removeClass("abt_contact_container"); 
+       $(".stephen .contact").addClass("row"); 
+       $(".stephen .contact .mailing").addClass("medium-6 columns"); 
+       $(".stephen .contact .social").addClass("medium-6 columns");
+    }else{
+       $(".stephen .contact").addClass("abt_contact_container"); 
+       $(".stephen .contact").removeClass("row"); 
+       $(".stephen .contact .mailing").removeClass("medium-6 columns"); 
+       $(".stephen .contact .social").removeClass("medium-6 columns"); 
+    }
+});
