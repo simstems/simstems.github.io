69d49cbafc7c0d82e8871e2f392241e9a83eb21f
Stephen Morrison <simstems@users.noreply.github.com>
2017-06-02 05:52:04 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).foundation();
 $(document).ready(function(){
+    // For about.html
     if($(window).innerWidth() <= 751) {
        $(".stephen .contact").removeClass("abt_contact_container"); 
        $(".stephen .contact").addClass("row"); 
@@ -12,3 +13,59 @@ $(document).ready(function(){
        $(".stephen .contact .social").removeClass("medium-6 columns"); 
     }
 });
+// For contact.html
+function verify() {
+    
+    function send(){
+        document.formspree.method ="POST";
+        document.formspree.action ="https://formspree.io/contact@simstems.com";
+        return true;
+    }
+
+    var errorBackground = '#fb7e7e', correctBackground = '#fff';
+    
+    $(".mail-button").focus(function(){
+        if ((document.formspree.email.value == "") && (document.formspree.subject.value == "") && (document.formspree.message.value == "")) {
+            $('.mail-form .email').css('background-color', errorBackground);
+            $('.mail-form .subject').css('background-color', errorBackground);
+            $('.mail-form .message').css('background-color', errorBackground);
+        }
+        else if ((document.formspree.email.value == "") && (document.formspree.subject.value == "")) {
+            $('.mail-form .email').css('background-color', errorBackground);
+            $('.mail-form .subject').css('background-color', errorBackground);
+            $('.mail-form .message').css('background-color', correctBackground);
+        }
+        else if ((document.formspree.email.value == "") && (document.formspree.message.value == "")) {
+            $('.mail-form .email').css('background-color', errorBackground);
+            $('.mail-form .message').css('background-color', errorBackground);
+            $('.mail-form .subject').css('background-color', correctBackground);
+        }
+        else if ((document.formspree.subject.value == "") && (document.formspree.message.value == "")) {
+            $('.mail-form .subject').css('background-color', errorBackground);
+            $('.mail-form .message').css('background-color', errorBackground);
+            $('.mail-form .email').css('background-color', correctBackground);
+        }
+        else if (document.formspree.message.value == "") {
+            $('.mail-form .message').css('background-color', errorBackground);
+            $('.mail-form .email').css('background-color', correctBackground);
+            $('.mail-form .subject').css('background-color', correctBackground);
+        }
+        else if (document.formspree.subject.value == "") {
+            $('.mail-form .subject').css('background-color', errorBackground);
+             $('.mail-form .email').css('background-color', correctBackground);
+            $('.mail-form .message').css('background-color', correctBackground);
+        }
+        else if (document.formspree.email.value == "") {
+            $('.mail-form .email').css('background-color', errorBackground);
+             $('.mail-form .subject').css('background-color', correctBackground);
+            $('.mail-form .message').css('background-color', correctBackground);
+        }
+        else {
+            $('.mail-form .email').css('background-color', correctBackground);
+            $('.mail-form .subject').css('background-color', correctBackground);
+            $('.mail-form .message').css('background-color', correctBackground);
+        }
+    });
+    return send();
+}
+verify();
