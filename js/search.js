4121317fc22fc8283e461a87dd1830f11381b0f7
Stephen Morrison <simstems@users.noreply.github.com>
2017-06-05 02:23:18 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("#search-button").click(function(){
-       $("#srch-close-btn, #search-bar, #search-button, #search-drawer").addClass("show"); 
+       $("#srch-close-btn, #search-bar, #search-button, #search-drawer").addClass("show");
+       $("#search-bar").focus();
     });
     $("#srch-close-btn, .menu-button").click(function(){
        $("#srch-close-btn, #search-bar, #search-button, #search-drawer").removeClass("show"); 
